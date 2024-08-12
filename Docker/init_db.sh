ee2c97fc9b2baad97799817c800d6b2cb73c94bd
Adrianibarra97 <adrian__ibarra@hotmail.com>
2024-08-11 23:58:20 -0300

    configure-postgress

--- Docker/init_db.sh
+++
@@ -1,8 +0,0 @@
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB"
-<<-EOSQL
-    CREATE DATABASE backend;
-    CREATE USER docker WITH ENCRYPTED PASSWORD 'docker';
-    GRANT ALL PRIVILEGES ON DATABASE backend TO docker;
-EOSQL
